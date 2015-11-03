e156d131f39cb9dcbd2f6254abec8a9a9a26643e
CaioRS <caiorss.rodrigues@gmail.com>
2015-11-03 12:26:48 -0300

    em.sh

---
+++ em.sh
@@ -0,0 +1,123 @@
+#!/usr/bin/env sh
+#
+#  Emacs Client Wrapper
+# 
+#  It helps to manage Emacs client and daemon.
+# 
+# 
+#  
+#
+#-----------------------------------------------
+
+BROWSER=chromium-browser
+
+case "$1" in
+
+    # Start Emacs Server
+    -start)
+        emacs --daemon ;;
+
+    -stop)
+        emacsclient --eval "(kill-emacs)" ;;
+        
+    -restart)
+        emacsclient --eval "(kill-emacs)" ;
+        emacs --daemon ;;
+
+    # Open Emacs Frame (GUI)
+    -gui)
+        if [ -z "$2"] ;
+        then
+             emacsclient -a "" -c
+        else
+             emacsclient -a "" -c "$2"
+        fi
+             ;;
+        
+    # Open Emacs in Terminal
+    -cli)
+        if [ -z "$2"] ;
+        then
+             emacsclient -a "" -t
+        else
+             emacsclient -a "" -t "$2"
+        fi
+             ;;
+        
+    # Execute Command (Eval)
+    -cmd)
+        emacsclient -a "" -n --eval "$2" ;;
+
+    # Load Emacs Lisp Source
+    -load)
+        emacsclient -a "" -n --eval "(load \"$2\")" ;;
+
+    -man)
+        emacsclient -a "" -t -e "(woman \"$2\")"  ;;
+
+    -manual)
+        $BROWSER "http://www.gnu.org/software/emacs/manual" ;;
+
+    -show)
+
+        case "$2" in
+             load-path)
+                 emacsclient -a ""   --eval "load-path" \
+                              | sed 's/["|\)|\(]//g' | sed 's/\s/\n/g'
+                 ;;
+
+            exec-path)
+                emacsclient -a ""   --eval "exec-path" \
+                    | sed 's/["|\)|\(]//g' | sed 's/\s/\n/g'
+                ;;
+        esac
+        ;;
+
+     ## List all files opened. 
+     ## 
+        -files)
+
+            emacsclient -a ""   --eval "(remove-if #'null (mapcar #'buffer-file-name (buffer-list)))" \
+                        | sed 's/["|\)|\(]//g' | sed 's/\s/\n/g'
+            ;;
+
+    *)
+
+echo "
+Emacs Sever/Client Manager (em)
+
+Usage:
+
+./em <option>
+
+ -start           Starts Emacs as sever, daemon.
+
+ -stop            Stop Emacs daemon.
+
+ -gui             Open a new Emacs frame
+
+ -cli             Open Emacs in terminal
+
+ -cmd <command>   Evaluates s-expression in Emacs, example:
+                  ./em.sh \"(+ 1 2 3 4 5)\"
+
+ -files           Show all files opened.
+ 
+ -load <file.el>  Load a Emacs Lisp source file.
+
+ -man <manpage>    Browser man page in terminal, example:
+                  ./em -man cbrt
+
+ -manual          Open Emacs Manual in Web Browser:
+
+ ------------------------------------------------
+ -show [option]   Get Emacs Information.
+
+       load-path  Show Emacs Load Path
+       exec-path  Show Emacs Exec Path
+
+ 
+"
+
+    ;;
+esac
